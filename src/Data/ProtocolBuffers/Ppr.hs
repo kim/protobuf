c034893cbe78f3c5d69e14f06a398e61e17ab8c6
Kim Altintop <kim@users.noreply.github.com>
2015-06-01 10:40:19 +0200

    First stab at reviving the pretty-printer

--- src/Data/ProtocolBuffers/Ppr.hs
+++ src/Data/ProtocolBuffers/Ppr.hs
@@ -1,23 +1,41 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE DeriveGeneric        #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE KindSignatures       #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Data.ProtocolBuffers.Ppr where
 
+import Data.ByteString              (ByteString)
 import Data.Int
-import Data.Word
+import Data.ProtocolBuffers.Message
 import Data.ProtocolBuffers.Types
 import Data.Proxy
-import Data.TypeLevel (Nat, D4, D5, D6, toInt)
--- import Data.Typeable
-import Text.PrettyPrint
+import Data.Text                    (Text)
+import Data.Word
 import GHC.Generics
+import GHC.TypeLits
+import Text.PrettyPrint
 
-data Foo = Foo {myField :: Required D4 Int64, theSecondField :: Optional D5 Float, aPackedField :: Packed D6 Int32} deriving (Show, Generic)
+
+data SomeEnum
+    = Enum1
+    | Enum2
+    | Enum3
+    deriving (Show, Bounded, Enum, Eq, Generic)
+
+data Foo = Foo
+    { a :: Required 4 (Value Int64)
+    , b :: Optional 5 (Value Float)
+    , c :: Packed   6 (Value Int32)
+    , d :: Repeated 7 (Message Foo)
+    , e :: Optional 8 (Enumeration SomeEnum)
+    } deriving (Show, Generic)
 
 prettyProto :: forall a . (Generic a, GPrettyProto (Rep a)) => Proxy a -> Doc
 prettyProto _ = gprettyProto (Proxy :: Proxy (Rep a))
@@ -26,37 +44,71 @@ class GPrettyProto (f :: * -> *) where
   gprettyProto :: Proxy f -> Doc
 
 instance (GPrettyProto f, Datatype d) => GPrettyProto (D1 d f) where
-  gprettyProto _ = text "message" <+> text (datatypeName (undefined :: t d f a)) $$ gprettyProto (Proxy :: Proxy f)
+  gprettyProto _
+      =   text "message"
+      <+> text (datatypeName (undefined :: t d f a))
+      $$  gprettyProto (Proxy :: Proxy f)
 
 instance (GPrettyProto f, Constructor c) => GPrettyProto (C1 c f) where
-  -- gprettyProto _ = text (conName (undefined :: t c f a)) $$ braces (gprettyProto (Proxy :: Proxy f))
   gprettyProto _ = braces (gprettyProto (Proxy :: Proxy f))
 
 instance (GPrettyProtoSel f, Selector s) => GPrettyProto (S1 s f) where
-  gprettyProto _ = gprettyProtoSel (Proxy :: Proxy f) selName' where
-    selName' = selName (undefined :: t s f a)
+  gprettyProto _ = gprettyProtoSel (Proxy :: Proxy f) selName'
+    where
+      selName' = selName (undefined :: t s f a)
 
 instance (GPrettyProto x, GPrettyProto y) => GPrettyProto (x :*: y) where
-  gprettyProto _ = gprettyProto (Proxy :: Proxy x) $+$ gprettyProto (Proxy :: Proxy y)
+  gprettyProto _ =
+      gprettyProto (Proxy :: Proxy x) $+$ gprettyProto (Proxy :: Proxy y)
 
 class GPrettyProtoSel (f :: * -> *) where
   gprettyProtoSel :: Proxy f -> String -> Doc
 
-instance (Nat n, ProtoTypeName c) => GPrettyProtoSel (K1 i (Required n c)) where
-  gprettyProtoSel _ selName' = text "required" <+> text (protoTypeName (Proxy :: Proxy c)) <+> text selName'' <+> text "=" <+> text (show (toInt (undefined :: n))) <> char ';' where
-    selName'' = if null selName' then "field" ++ show (toInt (undefined :: n)) else selName'
+instance (KnownNat n, ProtoTypeName a) => GPrettyProtoSel (K1 i (Field n (RequiredField (f a)))) where
+  gprettyProtoSel _ selName'
+      = text "required"
+      <+> text (protoTypeName (Proxy :: Proxy a))
+      <+> text selName''
+      <+> text "="
+      <+> text (show (natVal (Proxy :: Proxy n))) <> char ';'
+    where
+      selName'' | null selName' = "field" ++ show (natVal (Proxy :: Proxy n))
+                | otherwise     = selName'
 
-instance (Nat n, ProtoTypeName c) => GPrettyProtoSel (K1 i (Optional n c)) where
-  gprettyProtoSel _ selName' = text "optional" <+> text (protoTypeName (Proxy :: Proxy c)) <+> text selName'' <+> text "=" <+> text (show (toInt (undefined :: n))) <> char ';' where
-    selName'' = if null selName' then "field" ++ show (toInt (undefined :: n)) else selName'
+instance (KnownNat n, ProtoTypeName a) => GPrettyProtoSel (K1 i (Field n (OptionalField (f a)))) where
+  gprettyProtoSel _ selName'
+      = text "optional"
+      <+> text (protoTypeName (Proxy :: Proxy a))
+      <+> text selName''
+      <+> text "="
+      <+> text (show (natVal (Proxy :: Proxy n))) <> char ';'
+    where
+      selName'' | null selName' = "field" ++ show (natVal (Proxy :: Proxy n))
+                | otherwise     = selName'
 
-instance (Nat n, ProtoTypeName c) => GPrettyProtoSel (K1 i (Repeated n c)) where
-  gprettyProtoSel _ selName' = text "repeated" <+> text (protoTypeName (Proxy :: Proxy c)) <+> text selName'' <+> text "=" <+> text (show (toInt (undefined :: n))) <> char ';' where
-    selName'' = if null selName' then "field" ++ show (toInt (undefined :: n)) else selName'
+instance (KnownNat n, ProtoTypeName a) => GPrettyProtoSel (K1 i (Field n (RepeatedField (f a)))) where
+  gprettyProtoSel _ selName'
+      = text "repeated"
+      <+> text (protoTypeName (Proxy :: Proxy a))
+      <+> text selName''
+      <+> text "="
+      <+> text (show (natVal (Proxy :: Proxy n))) <> char ';'
+    where
+      selName'' | null selName' = "field" ++ show (natVal (Proxy :: Proxy n))
+                | otherwise     = selName'
 
-instance (Nat n, ProtoTypeName c) => GPrettyProtoSel (K1 i (Packed n c)) where
-  gprettyProtoSel _ selName' = text "repeated" <+> text (protoTypeName (Proxy :: Proxy c)) <+> text selName'' <+> text "=" <+> text (show (toInt (undefined :: n))) <+> text "[packed=true]" <> char ';' where
-    selName'' = if null selName' then "field" ++ show (toInt (undefined :: n)) else selName'
+instance (KnownNat n, ProtoTypeName a) => GPrettyProtoSel (K1 i (Field n (PackedField (f a)))) where
+  gprettyProtoSel _ selName'
+      = text "repeated"
+      <+> text (protoTypeName (Proxy :: Proxy a))
+      <+> text selName''
+      <+> text "="
+      <+> text (show (natVal (Proxy :: Proxy n)))
+      <+> text "[packed=true]" <> char ';'
+    where
+      selName'' | null selName' = "field" ++ show (natVal (Proxy :: Proxy n))
+                | otherwise     = selName'
+
 
 class ProtoTypeName a where
   protoTypeName :: Proxy a -> String
@@ -73,6 +125,21 @@ instance ProtoTypeName Word32 where
 instance ProtoTypeName Word64 where
   protoTypeName _ = "uint64"
 
+instance ProtoTypeName Float where
+  protoTypeName _ = "float"
+
+instance ProtoTypeName Double where
+  protoTypeName _ = "double"
+
+instance ProtoTypeName Bool where
+  protoTypeName _ = "bool"
+
+instance ProtoTypeName Text where
+  protoTypeName _ = "string"
+
+instance ProtoTypeName ByteString where
+  protoTypeName _ = "string"
+
 instance ProtoTypeName (Fixed Int32) where
   protoTypeName _ = "sfixed32"
 
@@ -85,8 +152,24 @@ instance ProtoTypeName (Fixed Word32) where
 instance ProtoTypeName (Fixed Word64) where
   protoTypeName _ = "fixed64"
 
-instance ProtoTypeName Float where
-  protoTypeName _ = "float"
+instance ProtoTypeName (Signed Int32) where
+  protoTypeName _ = "sint32"
 
-instance ProtoTypeName Double where
-  protoTypeName _ = "double"
+instance ProtoTypeName (Signed Int64) where
+  protoTypeName _ = "sint64"
+
+instance ProtoTypeName a => ProtoTypeName (Value a) where
+  protoTypeName _ = protoTypeName (Proxy :: Proxy a)
+
+-- this requires UndecidableInstances. any way to do without?
+instance MsgTypName (Rep a) => ProtoTypeName (Message a) where
+  protoTypeName _ = msgTypName (Proxy :: Proxy (Rep a))
+
+instance MsgTypName (Rep a) => ProtoTypeName (Enumeration a) where
+  protoTypeName _ = msgTypName (Proxy :: Proxy (Rep a))
+
+class MsgTypName (f :: * -> *) where
+  msgTypName :: Proxy f -> String
+
+instance (Datatype d) => MsgTypName (D1 d f) where
+  msgTypName _ = datatypeName (undefined :: t d f a)
